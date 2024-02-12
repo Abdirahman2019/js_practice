6c05bbc1c54a07e857434edbc59f6ef9bd303d76
Abdirahman <abdiraxman106@gmail.com>
2024-02-12 15:11:47 +0300

    modified index . js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const person ={
-    firstName: "oscar",
+const student ={
+    firstName: "ismail",
     lastName : "omar",
     Dob      : "12/03/2007",
     pob      : "guri",
     phone_number: 1234332
 }
-console.log(person.firstName);
+const school = {
+    school_name:"GSS",
+    class :"form 1H",
+    adm_no:43567,
+}
+console.log(student.firstName + " " + student.lastName + " is "+ school.class + " adm-no " + school.adm_no);
