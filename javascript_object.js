a6f43c55970e4154214adf6fc7f95f98ed32cd57
Abdirahman <abdiraxman106@gmail.com>
2024-02-12 15:15:46 +0300

    modified -java-oject file

--- javascript_object.js
+++ javascript_object.js
@@ -10,3 +10,16 @@ const person = {
   };
   console.log(person.firstName + " is " + person.age + " years old.");
   //The object (person) in the example above has 4 properties: firstName, lastName, age, and eyeColor.
+  const student ={
+    firstName: "ismail",
+    lastName : "omar",
+    Dob      : "12/03/2007",
+    pob      : "guri",
+    phone_number: 1234332
+}
+const school = {
+    school_name:"GSS",
+    class :"form 1H",
+    adm_no:43567,
+}
+console.log(student.firstName + " " + student.lastName + " is "+ school.class + " adm-no " + school.adm_no);
