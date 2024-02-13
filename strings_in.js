d249f4f542d431aa3944bc7076943fc369b9ae87
Abdirahman <abdiraxman106@gmail.com>
2024-02-13 22:12:10 +0300

    added file string

---
+++ strings_in.js
@@ -0,0 +1,28 @@
+/*
+Template Strings
+Templates were introduced with ES6 (JavaScript 2016).
+
+Templates are strings enclosed in backticks (`This is a template string`).
+
+Templates allow single and double quotes inside a string:
+
+Example
+let text = `He's often called "Johnny"`;
+
+
+String Length
+To find the length of a string, use the built-in length property:
+
+Example
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+
+Template Strings
+Templates were introduced with ES6 (JavaScript 2016).
+
+Templates are strings enclosed in backticks (`This is a template string`).
+
+Templates allow multiline strings:
+
+
+*/
